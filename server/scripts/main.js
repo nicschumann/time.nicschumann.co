8bbb61524ec923bb4c7606c83a75c3351d0b1c07
Nic Schumann <nicolas_schumann@brown.edu>
2018-12-25 14:43:47 -0500

    [pipeline] rebuilt main in new pipeline syntax.

--- server/scripts/main.js
+++ server/scripts/main.js
@@ -3,104 +3,66 @@
 var d3 = Object.assign( require('d3'), require('d3-fetch') );
 
 import { livereload } from './livereload-client.js';
-import { OptionsPane } from './components/options-pane/index.js';
-import { GroupDays } from './components/group-days/index.js';
-import { LogComponent } from './components/log/index.js';
-import { SummaryComponent } from './components/summary/index.js';
-import { NormalPlot } from './components/normal-plot/index.js';
-import { Lift } from './components/lift/index.js';
-// import { WeekSummaries } from './components/week-summary/index.js';
-// import { HourSummaries } from './components/hours-summary/index.js';
+
+
+import { GroupDays } from './components/transform-group-days.js';
+import { LogComponent } from './components/transform-log.js';
+import { SummaryComponent } from './components/transform-summarize.js';
+import { Each } from './components/transform-each.js';
+import { All } from './components/transform-all.js';
+
+import { NormalPlot } from './components/render-normal-plot.js';
+//import { OptionsPane } from './components/render-options-pane.js';
+
 
 livereload();
 
 console.log('main.js loaded, from gulp!');
 
-var days = new GroupDays();
+var days = new GroupDays( );
+var log = new LogComponent();
 var summarize = new SummaryComponent();
-// var normalplotA = new NormalPlot( document.querySelector('#left-selector') );
-//
-// var summarizeB = new Summary();
-var normalplotA = new NormalPlot( document.querySelector('#right-selector') );
-var log = new LogComponent()
 
-// var sizing = new Sizing();
-//
-// var optionsA = OptionsPane( document.querySelector('#left-selector') );
-// var optionsB = OptionsPane( document.querySelector('#right-selector') );
-//
-// // var weeks = WeekSummaries( document.querySelector('#main') );
-// //
-// // var hours = HourSummaries( document.querySelector('#main') );
-//
-// optionsA.init( sizing );
-// optionsB.init( sizing );
-//
-// d3  .json( '/api/v1/projects' )
-//     .then( function( res ) {
-//
-//         optionsA.source( res.data.projects );
-//
-//     });
-//
-// optionsA.sink( function( data ) {
-//     optionsB.source( data );
-// });
-
-//hours.init();
+var normalplotS = new NormalPlot( document.querySelector('#left-selector'), ['satisfaction'] );
+var normalplotP = new NormalPlot( document.querySelector('#middle-selector'), ['productivity'] );
+var normalplotE = new NormalPlot( document.querySelector('#right-selector'), ['enjoyment'] );
 
 days.init();
 log.init();
+summarize.init();
 
-normalplotA.init();
-// normalplotB.init();
+normalplotS.init();
+normalplotP.init();
+normalplotE.init();
 
-d3.json('/api/v1/entries/by-date/12-01-2018/12-11-2018')
-  .then( function( res ) {
+d3  .json('/api/v1/entries/by-date/12-01-2018/12-11-2018')
+    .then( function( res ) {
 
-      if ( res.success ) {
+        if ( res.success ) {
 
-          //console.log( res.data );
+            var pipeline = days
+                    .through( new Each( function( d ) { return d.entries; }) )
+                    .through( new Each( summarize.transform.bind( summarize ) ) )
+                    .through( new Each( function( d ) { return d[0]; } ) )
+                    .through( new LogComponent('Pipeline Root') );
 
-          days  .trigger( res.data.entries )
-                .through( new Lift( function( d ) { return d.entries; }) )
-                .through( new Lift( summarize.transform.bind( summarize ) ) )
-                .through( new Lift( function( d ) { return d[0]; } ) )
-                .through( normalplotA );
-                //.through( log );
+            pipeline
+                    .through( new LogComponent('Path A') )
+                    .through( normalplotS );
 
+            pipeline
+                    .through( new LogComponent('Path B') )
+                    .through( normalplotP );
 
-          // var source = days.source( res.data.entries );
-          //
-          //
-          // days.source( res.data.entries ).sink( function( data ) {
-          //
-          //     var d_prime = data.map( function( d ) {
-          //         return summarizeA.summarize( d.entries );
-          //     });
-          //
-          //     normalplotA.source( d_prime.slice( 1,4 ) );
-          //     normalplotB.source( d_prime.slice( 4,7 ) );
-          //
-          //     // summarizeA.source( data[3].entries ).sink( function( d ) {
-          //     //
-          //     //     normalplotA.source( d );
-          //     //
-          //     // });
-          //     //
-          //     // summarizeB.source( data[2].entries ).sink( function( d ) {
-          //     //
-          //     //     normalplotB.source( d );
-          //     //
-          //     // });
-          //
-          //
-          // });
+            pipeline
+                    .through( new LogComponent('Path B') )
+                    .through( normalplotE );
 
-      } else {
+            pipeline.run( res.data.entries );
 
-          console.error( res );
-      }
+        } else {
+            console.error( res );
+        }
 
-  })
-  .catch( function( error ) { console.error( error ); } );
+    })
+    .catch( function( error ) { console.error( error ); } );
